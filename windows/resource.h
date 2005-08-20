6ad9a5dbaae4e783c09a557f02631dec2ec5c082
mips <mips_act@users.sourceforge.net>
2005-08-20 22:02:12 +0000

    * Modify menu. go to added

--- windows/resource.h
+++ windows/resource.h
@@ -45,15 +45,15 @@
 #define ID_FILE_EXIT                    40004
 #define ID_HELP_ABOUT                   40005
 #define ID_TOOLS_VERIFYITEMS            40006
-#define ID_TOOLS_FINDMISSINGCLIENTITEMS 40007
 #define ID_TOOLS_FINDMISSINGITEMS       40007
+#define ID_TOOLS_GOTOITEM               40008
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        110
-#define _APS_NEXT_COMMAND_VALUE         40008
+#define _APS_NEXT_COMMAND_VALUE         40009
 #define _APS_NEXT_CONTROL_VALUE         1047
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
